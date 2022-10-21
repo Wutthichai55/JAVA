e8ad59463e10dac22de6bf3c8a4bf97391cd85bc
Wutthichai55 <potae10.2.47@gmail.com>
2022-10-21 11:08:09 +0700

    kuy
    ...........

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
-        
+        System.out.println("my name is potae!");
+        System.out.println("5555!");
     }
 }
